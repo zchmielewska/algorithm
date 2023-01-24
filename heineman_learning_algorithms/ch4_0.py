570d560f0b059529fd254dacb3fd7e58ca551fba
zchmielewska <zchmielewska@gmail.com>
2023-01-24 18:57:14 +0100

    challenge 4_1

--- heineman_learning_algorithms/ch4_0.py
+++ heineman_learning_algorithms/ch4_0.py
@@ -15,7 +15,7 @@ class Queue:
         return self.first is None
 
     def enqueue(self, val):
-        if self is None:
+        if self.first is None:
             self.first = self.last = Node(val)
         else:
             self.last.next = Node(val)
@@ -29,3 +29,57 @@ class Queue:
         self.first = self.first.next
         return val
 
+
+class Entry:
+    def __init__(self, v, p):
+        self.value = v
+        self.priority = p
+
+
+class PQ:
+    def less(self, i, j):
+        return self.storage[i].priority < self.storage[j].priority
+
+    def swap(self, i, j):
+        self.storage[i], self.storage[j] = self.storage[j], self.storage[i]
+
+    def __init__(self, size):
+        self.size = size
+        self.storage = [None] * (size+1)
+        self.N = 0
+
+    def enqueue(self, v, p):
+        if self.N == self.size:
+            raise RuntimeError("Priority queue is full.")
+
+        self.N += 1
+        self.storage[self.N] = Entry(v, p)
+        self.swim(self.N)
+
+    def swim(self, child):
+        while child > 1 and self.less(child//2, child):
+            self.swap(child, child//2)
+            child = child//2
+
+    def dequeue(self):
+        if self.N == 0:
+            raise RuntimeError("Priority queue is empty.")
+
+        max_entry = self.storage[1]
+        self.storage[1] = self.storage[self.N]
+        self.storage[self.N] = None
+        self.N -= 1
+        self.sink(1)
+        return max_entry.value
+
+    def sink(self, parent):
+        while 2*parent <= self.N:
+            child = 2*parent
+            if child < self.N and self.less(child, child+1):
+                child += 1
+
+            if not self.less(parent, child):
+                break
+
+            self.swap(child, parent)
+            parent = child
--- heineman_learning_algorithms/ch4_1_circular_queue.py
+++ heineman_learning_algorithms/ch4_1_circular_queue.py
@@ -1,3 +1,5 @@
+# Kolejka cykliczna
+
 class Queue:
     def __init__(self, size):
         self.size = size
